1e1b5dd5d0d3b74f16eee0bed08e65fa73bd2044
coreyjordanphx <corey.d.jordan66@gmail.com>
2022-05-18 11:24:59 -0500

    init commit

---
+++ geocoding.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+import requests
+from dotenv import dotenv_values
+from flask import Flask, render_template, request
+
+# Base Url for geocoding
+app = Flask(__name__)
+
+
+ENV = dotenv_values()
+@app.route('/', methods=["POST", "GET"])
+def index():
+    if request.method == "POST":
+        address = request.form['address_field']
+
+    #Your unique private_token should replace value of the private_token variable.
+    #To know how to obtain a unique private_token please refer the README file for this script.
+
+        data = {
+            'key': ENV['PRIVATE_TOKEN'],
+            'q': address,
+            'format': 'json'
+        }
+        response = requests.get(ENV['URL'], params=data)
+
+        latitude = response.json()[0]['lat']
+        longitude = response.json()[0]['lon']
+        
+        res = {
+            'message' : address,
+            'latitude': latitude,
+            'longitude': longitude
+        }
+    
+        return render_template('results.html', **res)
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8000, debug=True)
+ 
+
